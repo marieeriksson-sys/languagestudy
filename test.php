8a70602cb09de2996b5cf2565b8ac4dbad567655
marieeriksson-sys <marie.eriksson@elev.ga.lbs.se>
2026-04-16 14:20:52 +0200

    php

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+phpinfo();
+echo'hej';
+?>
+
+
+<form method="POST">
+    Titel: <input type="text" name="titel" required><br><br>
+    Beskrivning:<br>
+    <textarea name="beskrivning" rows="5" cols="40"></textarea><br><br>
+    <input type="submit" value="Skicka">
+</form>
