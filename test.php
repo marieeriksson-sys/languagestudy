741e0f9bc5e5c0cb8cbd3a929e66faec8fa88677
marieeriksson-sys <marie.eriksson@elev.ga.lbs.se>
2026-04-16 14:36:53 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,10 +3,19 @@ phpinfo();
 echo'hej';
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <form method="POST">
     Titel: <input type="text" name="titel" required><br><br>
     Beskrivning:<br>
     <textarea name="beskrivning" rows="5" cols="40"></textarea><br><br>
     <input type="submit" value="Skicka">
-</form>
+</form> 
+</body>
+</html>
