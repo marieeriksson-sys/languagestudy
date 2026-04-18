455c07c179e380fd5917ec9949092a23ee54e2ac
marieeriksson-sys <marie.eriksson@elev.ga.lbs.se>
2026-04-18 23:52:12 +0200

    Added the picture

--- php.php
+++ php.php
@@ -1,11 +1,14 @@
 <?php
-phpinfo();
 echo'hej';
+$conn = new mysqli("localhost", "Marie", "4653Zs7U", "databas");
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
